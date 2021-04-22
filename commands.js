e3b24b79367ad33de8d3f210a165b4d68d7eb788
jimmyrhythm <80177614+jimmyrhythm@users.noreply.github.com>
2021-04-22 16:24:48 -0400

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,165 @@
+var commands={
+  "Power":{label:"⏻",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Back":{label:"🠘",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Home":{label:"⌂",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Up":{label:"⯅",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Left":{label:"⯇",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Select":{label:"OK",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Right":{label:"⯈",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Down":{label:"⯆",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "InstantReplay":{label:"⟲",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Info":{label:"✱",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Rev":{label:"⯇⯇",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Play":{label:"⯈",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Fwd":{label:"⯈⯈",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "Search":{label:"⌕",
+            Description:"",
+            keycode:0,
+            groups:[" "]
+
+},
+  "Backspace":{label:"Bksp",
+            Description:"",
+            keycode:0,
+            groups:[""]
+
+},
+  "Enter":{label:"Enter",
+            Description:"",
+            keycode:0,
+            groups:[""]
+
+},
+  "VolumeUp":{label:"Vol+",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "VolumeMute":{label:"⊘",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "VolumeDown":{label:"Vol-",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "ChannelUp":{label:"CH +",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "ChannelDown":{label:"CH -",
+            Description:"",
+            keycode:0,
+            groups:["btn"]
+
+},
+  "InputTuner":{label:"Ant",
+            Description:"",
+            keycode:0,
+            groups:[""]
+
+},
+  "InputHDMI1":{label:"HDMI1",
+            Description:"",
+            keycode:0,
+            groups:[""]
+
+},
+  "InputHDMI2":{label:"HDMI2",
+            Description:"",
+            keycode:0,
+            groups:[""]
+
+},
+  "InputHDMI3":{label:"HDMI3",
+            Description:"",
+            keycode:0,
+            groups:[""]
+
+},
+  "InputHDMI4":{label:"HDMI4",
+            Description:"",
+            keycode:0,
+            groups:[""]
+
+},
+  "InputAV1":{label:"AV1",
+            Description:"",
+            keycode:0,
+            groups:[""]
+
+}
+};
+
